6c3912440e12557879f4852f4505d69961a50290
maliabrandt <malia.brandt@tufts.edu>
2024-12-08 22:24:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
+#Malia Brandt and Alex Wardwell 
+#December 2024
+#main.py , to be run on the raspberry pi pico
+#This is the main code for the hopscotch game. 
+
 from hopscotch import Hopscotch
 from machine import Pin
 import time, asyncio
 
+#create hopscotch instance 
 h = Hopscotch()
+
+#pin to communicate with the ESP-32C, when the game is finished, 
+#turn the pin high for one second. The ESP is listening for this signal.
 respond = Pin(21, Pin.OUT)
 
 
-
+#when button is pressed, start the game
 def callback(p):
     h.game_start_true()
     #print("starting")
+#button instance 
 
-p = Pin(20, Pin.IN, Pin.PULL_UP)  # guess with PULL_UP does...
+p = Pin(20, Pin.IN, Pin.PULL_UP)  
 p.irq(trigger=Pin.IRQ_RISING, handler=callback)
 
-
+#main run function for the program
 async def main():
     while True:
-        #play game
+        #play game when button is pressed
         while h.return_game_start():
             h.make_pattern()
             h.display_pattern()
@@ -28,7 +38,8 @@ async def main():
             respond.on()
             time.sleep(1)
             respond.off()
-            
+
+#run the main function to play the game
 asyncio.run(main())
         
         
