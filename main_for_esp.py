40fc6e7372d947c61a74395b4b2549d0bbf4d253
maliabrandt <malia.brandt@tufts.edu>
2024-12-08 22:27:03 -0500

    Update main_for_esp.py

--- main_for_esp.py
+++ main_for_esp.py
@@ -1,28 +1,41 @@
+#Malia Brandt and Alex Wardwel
+#December 2024
+#main.py for the ESP-32C mini 
+#This code listens for a signal from the pico, then published a "complete" message
+#for the interface box to receive. 
 from machine import Pin
 import time
 from now import Now
 
+#listening from the pico, the pin will turn high for one second when hopscotch is completed
 input_signal = Pin(1, Pin.IN)
+
+#led indicator
 on = Pin(2, Pin.OUT)
 on.value(0)
 
 n = Now()
 
+#turn led indicator on when connected, off when not
 try:
     n.connect()
     on.value(1)
 except:
     on.value(0)
 
+#main loop 
 while True:
     if input_signal.value() == 1:
+        #send publish signal
         n.publish(b'3complete')
         time.sleep(0.1)
+        #send random message afterwards (at the request of the interface team)
         n.publish(b'wergdfjwejif')
     time.sleep(0.1)
 
-    # Ensure interfaces are deactivated on exit
+# Ensure interfaces are deactivated on exit
 n.close()
+#turn led off 
 on.value(0)
 
 
